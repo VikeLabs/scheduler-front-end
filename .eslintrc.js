6b74f6f686113cfdcdac8d2c5c1e8422e80109ba
Joel Kerfoot <joelkerfoot@gmail.com>
2019-10-31 22:03:13 -0700

    setup eslint prettier and travis ci

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends:  [
+    'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions:  {
+  ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+  sourceType:  'module',  // Allows for the use of imports
+  ecmaFeatures:  {
+    jsx:  true,  // Allows for the parsing of JSX
+  },
+  },
+  rules:  {
+    'no-console': 'error',
+    'no-alert': 'error',
+    'require-jsdoc': 'error',
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+  },
+  settings:  {
+    react:  {
+      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
