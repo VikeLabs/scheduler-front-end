6e706ee6d2b6139555e8f6d05e34f0b438a05302
Joel Kerfoot <joelkerfoot@gmail.com>
2020-01-24 16:55:53 -0800

    cleaned up create react app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Calendar from './atoms/Calendar/Calendar';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Schedule Courses V2</p>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calendar />
     </div>
   );
 };
