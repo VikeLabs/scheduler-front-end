93ee5f78158eb690f54a528f8542a6507df73ad3
Rafay <abdurrafay073@gmail.com>
2020-02-07 15:08:02 -0800

    27: is integrated but needs clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Calendar from './atoms/Calendar/Calendar';
+import ExpansionPanel from './atoms/ExpansionPanel/ExpansionPanel';
 
 const App: React.FC = () => {
   return (
     <div>
       <Calendar />
+      <ExpansionPanel/>
     </div>
   );
 };
