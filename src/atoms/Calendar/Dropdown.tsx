9e5d97bbbaa299b3542e9f6184933f2eadf7f554
Nicole Peverley <Nicole@Nicoles-MacBook-Air-3.local>
2020-02-10 23:34:54 -0800

    23: Fix linting

--- src/atoms/Calendar/Dropdown.tsx
+++ src/atoms/Calendar/Dropdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import 'isomorphic-fetch';
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
@@ -5,84 +6,90 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 interface CourseType {
-    subject: string;
-    code: string;
+  subject: string;
+  code: string;
 }
- 
+
 const Dropdown: React.FC = () => {
-    const [open, setOpen] = React.useState(false);
-    const [options, setOptions] = React.useState<CourseType[]>([]);
-    const loading = open && options.length === 0;
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState<CourseType[]>([]);
+  const loading = open && options.length === 0;
 
-    function handleAddCourse(event: any, value: any) {
-      // TODO: Save the courses added by a user
-      return value
-    }
-  
-    React.useEffect(() => {
-      let active = true;
-  
-      if (!loading) {
-        return undefined;
-      }
-  
-      (async () => {
-        // TODO: Import the user's active termCode this is a placeholder
-        const termCode = 201901
-        const response = await fetch(`https://uvic-scheduler-server.herokuapp.com/courses?term=${termCode}`);
-        const courses = await response.json();
-        const space = " " as any
-  
-        if (active) {
-          setOptions(Object.keys(courses).map(key => courses[key].subject + space + courses[key].code) as CourseType[]);
-        }
-      })();
-  
-      return () => {
-        active = false;
-      };
-    }, [loading]);
-  
-    React.useEffect(() => {
-      if (!open) {
-        setOptions([]);
-      }
-    }, [open]);
-
-    return (
-      <Autocomplete
-        id="asynchronous-dropdown"
-        style={{ width: 300 }}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
-        }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        onChange={handleAddCourse}
-        getOptionSelected={(option , value) => option === value}
-        options={options}
-        loading={loading}
-        renderInput={params => (
-          <TextField
-            {...params}
-            label="Courses"
-            fullWidth
-            variant="outlined"
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
-            }}
-          />
-        )}
-      />
-    );
+  // TODO: Save the courses added by a user
+  /**
+  Adds course to user.
+  @param {any} _event The onChange event.
+  @param {any} num2 The value of the input.
+  @returns {any} The course that has been added.
+ */
+  function handleAddCourse(_event: any, value: any) {
+    return value;
   }
 
-  export default Dropdown;
+  React.useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      // TODO: Import the user's active termCode this is a placeholder
+      const termCode = 201901;
+      const response = await fetch(`https://uvic-scheduler-server.herokuapp.com/courses?term=${termCode}`);
+      const courses = await response.json();
+      const space = ' ' as any;
+
+      if (active) {
+        setOptions(Object.keys(courses).map(key => courses[key].subject + space + courses[key].code) as CourseType[]);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  return (
+    <Autocomplete
+      id="asynchronous-dropdown"
+      style={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      onChange={handleAddCourse}
+      getOptionSelected={(option, value) => option === value}
+      options={options}
+      loading={loading}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="Courses"
+          fullWidth
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default Dropdown;
--- src/atoms/Calendar/__tests__/Dropdown.test.tsx
+++ src/atoms/Calendar/__tests__/Dropdown.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Dropdown from '../Dropdown';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import Dropdown from '../Dropdown';
 
 configure({ adapter: new Adapter() });
 
