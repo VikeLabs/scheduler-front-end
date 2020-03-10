69cfbe14f7b8c3211f97851459a89baa8210fe0f
Joel Kerfoot <joelkerfoot@gmail.com>
2020-03-09 21:39:24 -0700

    removed tsx files

--- src/atoms/Theme/Theme.tsx
+++
@@ -1,9 +0,0 @@
-const Theme = {
-  primary: 'midnightblue',
-  bgLight: 'rgb(238,244,251)',
-  secondary: 'cornflowerblue',
-  gray: 'lightslategrey',
-  white: 'white',
-};
-
-export default Theme;
