7efa383a0bcff8d2bd217533b0b3c8983e96e92f
Dominique Charlebois <domo.charlebois@gmail.com>
2020-05-23 23:48:21 -0700

    Update Home Page - Add Grid Layout from Material UI and Responsive Design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import SignInForm from 'organisms/SignInForm/SignInForm';
 import SignUpForm from 'organisms/SignUpForm/SignUpForm';
 import logo from 'assets/logo.png';
-import { SignupBtn, StyledCard } from './styles';
+import { StyledCard } from './styles';
 import './Home.css';
 
 const Home = props => {
@@ -11,10 +13,6 @@ const Home = props => {
   const signinForm = (
     <>
       <SignInForm />
-      <div className="signup-container">
-        <span className="signup-text">New to Schedule Courses?</span>
-        <SignupBtn href="/signup">Create Account</SignupBtn>
-      </div>
     </>
   );
   const signupForm = (
@@ -23,18 +21,21 @@ const Home = props => {
     </>
   );
   return (
-    <main className="main-container">
-      <div className="left-container">
-        <img src={logo} className="logo" alt="" />
-      </div>
-      <div className="right-container">
-        <StyledCard>
-          <h1>Schedule Courses</h1>
-          <h3>Time Table Builder for UVic Students</h3>
-          {signin ? signinForm : signupForm}
-        </StyledCard>
-      </div>
-    </main>
+    <Grid container component="main" className="home-page">
+      <CssBaseline />
+      <Grid item xs={12} sm={12} md={6} component="section" className="left-section">
+        <img src={logo} alt="Vike Labs" />
+      </Grid>
+      <Grid item xs={12} sm={12} md={6} component="section" className="right-section">
+        <div className="form-section">
+          <StyledCard>
+            <h1>Schedule Courses</h1>
+            <h3>Time Table Builder for UVic Students</h3>
+            {signin ? signinForm : signupForm}
+          </StyledCard>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
